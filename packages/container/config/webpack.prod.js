4a7ae4af6c755b16c7718991c35b0079a535094a
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2024-06-16 07:36:39 +0100

    Change the deploy script for container

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -7,7 +7,8 @@ const domain = process.env.PRODUCTION_NAME
 const prodConfig = {
   mode: 'production',
   output: {
-    filename: '[name].[contenthash].js' 
+    filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/' 
   },
   plugins: [
     new ModuleFederationPlugin({
