ee941d6d9451832357d3c84806cd43285e626ceb
gabrielhyimah <gabrielamanfogyimah@gmail.com>
2024-06-16 09:33:02 +0100

    Added marketing deployment and webpack configurations

--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -2,11 +2,13 @@ const { merge } = require('webpack-merge')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common')
 const packageJson = require('../package.json')
+const domain = process.env.PRODUCTION_NAME
 
 const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/marketing/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
